c64a2d08734beddd21177edeaa52966f7b7251b5
juthaaa <juthatawat.sa@gmail.com>
2019-10-04 10:48:26 +0700

    [Delete] Middleware non-use

--- middlewares/middlewareIndex.js
+++ middlewares/middlewareIndex.js
@@ -1,16 +1,7 @@
 const express = require('express');
 const router = express.Router();
 var middlewareBodyParser = require('./middlewareBodyParser');
-// var middlewareMySql = require('./middlewareMysql');
-// var middlewareJWT = require('./middlewareJWT');
-// var middlewareMongodb = require('./middlewareMongodb');
-// var middlewareMorgan = require('./middlewareMorgan');
 
 router.use('/', middlewareBodyParser);
-// router.use('/', middlewareMySql);
-// router.use('/cms', middlewareJWT);
-// router.use('/um/logout', middlewareJWT);
-// router.use('/', middlewareMongodb);
-// router.use('/', middlewareMorgan);
 
 module.exports = router;
